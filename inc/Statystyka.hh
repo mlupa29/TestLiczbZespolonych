66cac5d0c100333d2390fc91e62aaf5a7db8f8d2
Michał Lupa <michal.lupa4@wp.pl>
2020-03-29 21:29:02 +0200

    gotowy program

--- inc/Statystyka.hh
+++ inc/Statystyka.hh
@@ -11,11 +11,11 @@ struct Statystyka
     double procent;
 };
 
-void Wypisz(Statystyka &St);
-void Zeruj(Statystyka &St);
-void Dodaj_d(Statystyka &St);
-void Dodaj_z(Statystyka &St);
-int Suma(Statystyka& St);
-int Ile_Dobrych(Statystyka &St);
-double Procent(Statystyka &St);
+void Wypisz(Statystyka  &St);
+void Zeruj(Statystyka  &St);
+void Dodaj_d(Statystyka  &St);
+void Dodaj_z(Statystyka  &St);
+int Suma(Statystyka  &St);
+int Ile_Dobrych(Statystyka  &St);
+double Procent(Statystyka  &St);
 #endif // STATYSTYKA_HH
