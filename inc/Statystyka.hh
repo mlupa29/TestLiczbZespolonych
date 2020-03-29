ac1490d1a10e33aac91baebb3a6009cd31e2e681
Michał Lupa <michal.lupa4@wp.pl>
2020-03-29 16:45:02 +0200

    gotowy program

--- inc/Statystyka.hh
+++ inc/Statystyka.hh
@@ -15,7 +15,7 @@ void Wypisz(Statystyka &St);
 void Zeruj(Statystyka &St);
 void Dodaj_d(Statystyka &St);
 void Dodaj_z(Statystyka &St);
-int Suma(Statystyka &St)
+int Suma(Statystyka& St);
 int Ile_Dobrych(Statystyka &St);
 double Procent(Statystyka &St);
 #endif // STATYSTYKA_HH
